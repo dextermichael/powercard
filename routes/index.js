6eba166becc980d44cb7755d1c3ebece2fa3c5c2
dextermichaelh <dextermichael.h@gmail.com>
2018-11-06 12:40:48 -0500

    heroku test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const applicationController = require('../controller/Application')
-const usersController = require('../controller/Users')
-const skillsController = require('../controller/Skills')
-const tasksController = require('../controller/Tasks')
-// const bodyparser = require('body-parser')
+const applicationController = require("../controller/application")
+const usersController = require('../controller/users')
+const skillsController = require('../controller/skills')
+const tasksController = require('../controller/tasks')
 
 
 
@@ -15,41 +14,41 @@ router.get('/', applicationController.index)
 // router.get('/users',usersController.index)
 
 
-// //User controller 
-// router.get('/users',usersController.index)
-// // 
-// router.get('/users/new',usersController.new)
-// //show
-// router.get('/users/:id',usersController.show)
-// //
-// router.get('/users/:id/edit',usersController.edit)
-// //create
-// router.post('/users',usersController.create)
-// // delete
-// router.delete('/users/:id',usersController.delete)
-// // update
-// router.patch('/users/:id',usersController.update)
+//User controller 
+router.get('/users',usersController.index)
+// 
+router.get('/users/new',usersController.new)
+//show
+router.get('/users/:id',usersController.show)
+//
+router.get('/users/:id/edit',usersController.edit)
+//create
+router.post('/users',usersController.create)
+// delete
+router.delete('/users/:id',usersController.delete)
+// update
+router.patch('/users/:id',usersController.update)
 
 
-// // //Skill
+// //Skill
 
-// router.get ('/skills',skillsController.index)
-// router.get ('/skills/new',skillsController.new)
-// router.get ('/skills/:id',skillsController.show)
-// router.get('/skills/:id/edit',skillsController.edit) 
-// router.post('/skills',skillsController.create) 
-// router.patch('/skills/:id',skillsController.update)
-// router.delete('/skills/:id',skillsController.delete)
+router.get ('/skills',skillsController.index)
+router.get ('/skills/new',skillsController.new)
+router.get ('/skills/:id',skillsController.show)
+router.get('/skills/:id/edit',skillsController.edit) 
+router.post('/skills',skillsController.create) 
+router.patch('/skills/:id',skillsController.update)
+router.delete('/skills/:id',skillsController.delete)
 
 
-// // //task
+// //task
 
-// router.get('/tasks',tasksController.index)
-// router.get ('/tasks/new',tasksController.new)
-// router.get ('/tasks/:id',tasksController.show)
-// router.post('/tasks',tasksController.create) 
-// router.get('/tasks/:id/edit',tasksController.edit) 
-// router.patch('/tasks/:id',tasksController.update)
+router.get('/tasks',tasksController.index)
+router.get ('/tasks/new',tasksController.new)
+router.get ('/tasks/:id',tasksController.show)
+router.post('/tasks',tasksController.create) 
+router.get('/tasks/:id/edit',tasksController.edit) 
+router.patch('/tasks/:id',tasksController.update)
 
 
 
